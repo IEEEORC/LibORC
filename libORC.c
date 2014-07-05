6459e39fcd8452a1426044c8aa7d8bd65a860fbd
Bobby <robert.bobby.wood@gmail.com>
2014-07-05 18:21:28 -0400

    Added basic empty functions that are being used in the EV3toC program.

---
+++ libORC.c
@@ -0,0 +1,15 @@
+#include "libORC.h"
+#include <arduino.h>
+
+void moveTankRot(int speedA, int speedB, double nrot) {
+
+}
+long readColourSensor(int port) {
+
+}
+long readDistanceCM() {
+
+}
+long readDistanceInches() {
+
+}
---
+++ libORC.h
@@ -0,0 +1,5 @@
+void moveTankRot(int speedA, int speedB, double nrot);
+long readColourSensor(int port);
+long readDistanceCM();
+long readDistanceInches();
+
